c6a33e652e555126bfbe61e488e224cfa9e80965
david-rojo <davidrojotech@gmail.com>
2021-10-26 20:32:01 +0200

    add logs in controller

--- src/main/java/com/mastercloudapps/twitterscheduler/controller/IndexApiController.java
+++ src/main/java/com/mastercloudapps/twitterscheduler/controller/IndexApiController.java
@@ -1,5 +1,8 @@
 package com.mastercloudapps.twitterscheduler.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,9 +16,18 @@ import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 @SecurityRequirement(name = "twitter-scheduler")
 public class IndexApiController {
 
+	@Value("${openapi.credentials.user}")
+	private String user;
+	
+	@Value("${openapi.credentials.password}")
+	private String password;
+
+	private Logger log = LoggerFactory.getLogger(IndexApiController.class);
+	
 	@GetMapping
 	public ResponseEntity<String> getIndex() {
 
+		log.info("user: " + user + ", password: " + password);
 		return new ResponseEntity<>("Hello from TwitterScheduler app!", HttpStatus.OK);
 	}
 }
