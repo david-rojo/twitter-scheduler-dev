e0e3a77df717948c887b2db4789232c103e5ad91
david-rojo <davidrojotech@gmail.com>
2021-10-26 20:13:01 +0200

    add openapi and info controller

---
+++ src/main/java/com/mastercloudapps/twitterscheduler/controller/IndexApiController.java
@@ -0,0 +1,21 @@
+package com.mastercloudapps.twitterscheduler.controller;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+
+@RestController
+@RequestMapping("/")
+@SecurityRequirement(name = "twitter-scheduler")
+public class IndexApiController {
+
+	@GetMapping
+	public ResponseEntity<String> getIndex() {
+
+		return new ResponseEntity<>("Hello from TwitterScheduler app!", HttpStatus.OK);
+	}
+}
