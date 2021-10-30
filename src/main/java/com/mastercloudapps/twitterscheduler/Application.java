f09f7db67a943af03e60d79171f34938ff0c1cc8
David Rojo <david-rojo@users.noreply.github.com>
2021-10-30 02:09:21 +0200

    add togglz (#1)

--- src/main/java/com/mastercloudapps/twitterscheduler/Application.java
+++ src/main/java/com/mastercloudapps/twitterscheduler/Application.java
@@ -2,6 +2,11 @@ package com.mastercloudapps.twitterscheduler;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.togglz.core.manager.EnumBasedFeatureProvider;
+import org.togglz.core.spi.FeatureProvider;
+
+import com.mastercloudapps.twitterscheduler.configuration.featureflags.Features;
 
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
 import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
@@ -15,4 +20,9 @@ public class Application {
 		SpringApplication.run(Application.class, args);
 	}
 
+	@Bean
+	public FeatureProvider featureProvider() {
+		
+	    return new EnumBasedFeatureProvider(Features.class);
+	}
 }
