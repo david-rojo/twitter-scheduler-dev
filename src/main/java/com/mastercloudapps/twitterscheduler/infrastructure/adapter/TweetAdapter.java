bcffad3c6692f26055de9c1af503181069d4477d
david-rojo <davidrojotech@gmail.com>
2021-11-15 00:58:29 +0100

    clear log traces

--- src/main/java/com/mastercloudapps/twitterscheduler/infrastructure/adapter/TweetAdapter.java
+++ src/main/java/com/mastercloudapps/twitterscheduler/infrastructure/adapter/TweetAdapter.java
@@ -4,8 +4,6 @@ import java.util.Collection;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.dao.DataAccessException;
 import org.springframework.stereotype.Component;
 
@@ -21,8 +19,6 @@ public class TweetAdapter implements TweetPort {
 
 	private static final String DATA_ACCESS_ERROR = "An unexpected error occurred executing a data access operation";
 	
-	private static Logger logger = LoggerFactory.getLogger(TweetAdapter.class);
-
 	private TweetJpaRepository tweetJpaRepository;
 	
 	private TweetJpaMapper mapper;
@@ -71,14 +67,11 @@ public class TweetAdapter implements TweetPort {
 	@Override
 	public Optional<Tweet> findOne(Long id) {
 		
-		logger.info("searching tweet with id " + id + " in database");
 		final var tweetJpa = tweetJpaRepository.findById(id);
 
 		if (tweetJpa.isPresent()) {
-			logger.info("found one in db");
 			return Optional.of(mapper.mapEntity(tweetJpa.get()));
 		}
-		logger.info("not found one in db");
 		return Optional.empty();
 	}
 
