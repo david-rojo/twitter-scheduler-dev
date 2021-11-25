bc69c3d068a1d67774f2d9ba20eabfb40903afcd
david-rojo <davidrojotech@gmail.com>
2021-11-25 01:55:19 +0100

    add images db tables

---
+++ src/main/java/com/mastercloudapps/twitterscheduler/infrastructure/jpa/pending/PendingImageJpaEntity.java
@@ -0,0 +1,30 @@
+package com.mastercloudapps.twitterscheduler.infrastructure.jpa.pending;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@Builder
+@NoArgsConstructor
+@Table(name="PENDING_IMAGE")
+public class PendingImageJpaEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(columnDefinition="TEXT")
+	private String url;
+
+}
